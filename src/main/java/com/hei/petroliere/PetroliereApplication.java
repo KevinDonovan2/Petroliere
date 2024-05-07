a5896a6fc61c10e3ee1690db983a33c96d5760dc
KevinDonovan2 <hei.donovan.2@gmail.com>
2024-05-07 14:57:56 +0300

    init project

---
+++ src/main/java/com/hei/petroliere/PetroliereApplication.java
@@ -0,0 +1,13 @@
+package com.hei.petroliere;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetroliereApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetroliereApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hei/petroliere/PetroliereApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hei.petroliere;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetroliereApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
